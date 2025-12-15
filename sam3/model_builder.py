023b00010f77616f99a87c1b1c4f0ff01cf4f969
Christopher Field <chris.field@theiascientific.com>
2025-12-15 17:14:23 -0500

    Change to importlib to fix warning

--- sam3/model_builder.py
+++ sam3/model_builder.py
@@ -2,11 +2,13 @@
 
 from typing import Optional
 
-import pkg_resources
-
+import atexit
 import torch
 import torch.nn as nn
+
+from contextlib import ExitStack
 from huggingface_hub import hf_hub_download
+from importlib import resources
 from iopath.common.file_io import g_pathmgr
 from sam3.model.decoder import (
     TransformerDecoder,
@@ -581,9 +583,10 @@ def build_sam3_image_model(
         A SAM3 image model
     """
     if bpe_path is None:
-        bpe_path = pkg_resources.resource_filename(
-            "sam3", "assets/bpe_simple_vocab_16e6.txt.gz"
-        )
+        file_manager = ExitStack()
+        atexit.register(file_manager.close)
+        ref = resources.files("sam3") / "assets/bpe_simple_vocab_16e6.txt.gz"
+        bpe_path = str(file_manager.enter_context(resources.as_file(ref)))
 
     # Create visual components
     compile_mode = "default" if compile else None
@@ -670,9 +673,10 @@ def build_sam3_video_model(
         Sam3VideoInferenceWithInstanceInteractivity: The instantiated dense tracking model
     """
     if bpe_path is None:
-        bpe_path = pkg_resources.resource_filename(
-            "sam3", "assets/bpe_simple_vocab_16e6.txt.gz"
-        )
+        file_manager = ExitStack()
+        atexit.register(file_manager.close)
+        ref = resources.files("sam3") / "assets/bpe_simple_vocab_16e6.txt.gz"
+        bpe_path = str(file_manager.enter_context(resources.as_file(ref)))
 
     # Build Tracker module
     tracker = build_tracker(apply_temporal_disambiguation=apply_temporal_disambiguation)
